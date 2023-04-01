1ae8caa5bdb7a1ec194caa34d4575fb1d63ca9b1
Brandon <brandiicoetzee@gmail.com>
2023-04-01 02:38:28 +0200

    added: typescript

--- nvim/lua/plugin-configs/nvim-treesitter.lua
+++ nvim/lua/plugin-configs/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'c', 'cpp', 'lua', 'bash', 'vim', 'go', 'javascript', 'python' },
+    ensure_installed = { 'c', 'cpp', 'lua', 'bash', 'vim', 'go', 'javascript', 'python', 'typescript' },
     sync_install = false,
     auto_install = true,
     highlight = {
